1a60ac14b616a339f482ca644438da9e03f42640
Juan <juan.rodriguez@edosoft.es>
2025-04-25 09:35:02 +0100

    Adds database and dependency injection support
    Configures the module to support database interaction and
    dependency injection by adding required modules and exports
    packages for external use.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,10 +5,12 @@ module com.juanrdzbaeza.javaphotowidget {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
-    
+    requires java.sql;
+    requires spring.beans;
+    requires spring.context;
 
     opens com.juanrdzbaeza.javaphotowidget to javafx.fxml;
     exports com.juanrdzbaeza.javaphotowidget;
-
-
+    exports com.juanrdzbaeza.javaphotowidget.api;
+    opens com.juanrdzbaeza.javaphotowidget.api to javafx.fxml;
 }
